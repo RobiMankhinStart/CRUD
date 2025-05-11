6a87978b89a78ac7ca23a4f3571fa8fb75600467
RobiMankhinStart <mankhinrobi@gmail.com>
2025-05-12 00:42:22 +0600

    added search option

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { removeContact } from "../Redux/NameSlice";
@@ -6,17 +6,29 @@ import { removeContact } from "../Redux/NameSlice";
 const Home = () => {
   const dispatch = useDispatch();
   const contacts = useSelector((state) => state.first);
+  const [search, setSearch] = useState("");
+  const handleSearch = (value) => {
+    setSearch(value);
+  };
   return (
     <div className="mt-10">
       <h2 className="text-center text-2xl font-bold text-zinc-600">
         Let's CRUD
       </h2>
-      <Link
-        className="ml-[460px] font-semibold text-2xl mt-12 p-2 rounded-md flex w-[200px] justify-center text-white shadow-lg bg-blue-500 ease-linear transition-all delay-100 hover:bg-white border hover:border-blue-700 hover:text-blue-700 "
-        to="/create"
-      >
-        Create +
-      </Link>
+      <div className="mt-12 flex justify-center gap-[240px]">
+        <Link
+          className=" font-semibold text-2xl  p-2 rounded-md  text-white shadow-lg bg-blue-500 ease-linear transition-all delay-100 hover:bg-white border hover:border-blue-700 hover:text-blue-700 "
+          to="/create"
+        >
+          Create +
+        </Link>
+        <input
+          onChange={(e) => handleSearch(e.target.value)}
+          className="w-[240px] border border-gray-300 p-2 rounded-md outline-none"
+          type="text"
+          placeholder="Search here...."
+        />
+      </div>
       <table className="mt-3 mx-auto min-w-[600px] border border-gray-300 shadow-md rounded-lg overflow-hidden">
         <thead>
           <tr className="bg-gray-200 text-left text-gray-700 uppercase text-sm">
@@ -27,34 +39,42 @@ const Home = () => {
           </tr>
         </thead>
         <tbody>
-          {contacts.map((value) => (
-            <tr
-              key={value.id}
-              className="hover:bg-gray-100 transition-colors duration-200"
-            >
-              <td className="py-2 px-4 border-b border-gray-200">{value.id}</td>
-              <td className="py-2 px-4 border-b border-gray-200">
-                {value.name}
-              </td>
-              <td className="py-2 px-4 border-b border-gray-200">
-                {value.email}
-              </td>
-              <td className="py-2 px-4 border-b border-gray-200 flex gap-2">
-                <Link
-                  to={`/update/${value.id}`}
-                  className="text-blue-700  hover:bg-blue-600 hover:text-white border border-blue-700 transition rounded duration-150 px-2 py-1 font-medium"
-                >
-                  Update
-                </Link>
-                <button
-                  onClick={() => dispatch(removeContact(value.id))}
-                  className="text-red-600 hover:text-white hover:bg-red-500 border border-red-500 px-2 py-1 rounded transition duration-150"
-                >
-                  Remove
-                </button>
-              </td>
-            </tr>
-          ))}
+          {contacts
+            .filter(
+              (contact) =>
+                contact.name.toLowerCase().includes(search.toLowerCase()) ||
+                contact.email.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((value) => (
+              <tr
+                key={value.id}
+                className="hover:bg-gray-100 transition-colors duration-200"
+              >
+                <td className="py-2 px-4 border-b border-gray-200">
+                  {value.id}
+                </td>
+                <td className="py-2 px-4 border-b border-gray-200">
+                  {value.name}
+                </td>
+                <td className="py-2 px-4 border-b border-gray-200">
+                  {value.email}
+                </td>
+                <td className="py-2 px-4 border-b border-gray-200 flex gap-2">
+                  <Link
+                    to={`/update/${value.id}`}
+                    className="text-blue-700  hover:bg-blue-600 hover:text-white border border-blue-700 transition rounded duration-150 px-2 py-1 font-medium"
+                  >
+                    Update
+                  </Link>
+                  <button
+                    onClick={() => dispatch(removeContact(value.id))}
+                    className="text-red-600 hover:text-white hover:bg-red-500 border border-red-500 px-2 py-1 rounded transition duration-150"
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
